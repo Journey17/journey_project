70d845579cf81dd0c8fd399c159feb9730bc25ab
hello <jlypersonal@outlook.com>
2023-03-21 16:51:34 +0800

    up

--- project_1/js/js.js
+++ project_1/js/js.js
@@ -52,10 +52,42 @@ function andx(a,b) {
 
 document.getElementById('andx').innerHTML = andx(4,5);
 
-// 01 引用 http://caibaojian.com/javascript/10.html
-
 // ------------------------------------------------------
 
 var y = "5",x = 5,z = 11;
 var w = y > z ? "hello" : "hell"
 console.log(w);
+
+var aa = "http://127.0.0.1:5500/project_1/";
+var dd = window.location.href == aa ? getDate() : "no" ;
+// console.log(dd);
+
+function getDate(){
+	var date = new Date();
+    var year = date.getFullYear();    //  返回的是年份
+    var month = date.getMonth() + 1;  //  返回的月份上个月的月份，记得+1才是当月
+    var dates = date.getDate();       //  返回的是几号
+    var day = date.getDay();          //  周一返回的是1，周六是6，但是周日是0
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var arr = [ "星期日","星期一","星期二","星期三","星期四","星期五","星期六",];
+    document.getElementById("time").innerHTML =
+      "今天是 " +
+      year +
+      " 年 " +
+      month +
+      " 月 " +
+      dates +
+      " 日 " +
+      arr[day] +
+      hours +
+      " 时 " +
+      minutes +
+      " 分 " +
+      seconds +
+      " 秒 ";
+}
+
+// 引用02 http://caibaojian.com/javascript/12.html
+
